ffbf5f1f07328e94857e4cbbbb203d816dced4fe
Stanislaus Cristo DLR <stanislauscristo_dlr@pop-os.localdomain>
2022-04-21 14:10:36 +0200

    commit for gh-pages

---
+++ _site/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.carousel').carousel();
+    $('.dropdown-trigger').dropdown();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ index.md
@@ -0,0 +1,5 @@
+---
+layout: home
+---
+
+A community diagnostic and performance metrics tool for routine evaluation of Earth system models in CMIP
